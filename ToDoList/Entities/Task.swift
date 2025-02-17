3bbf000a7732025bd5225e1b892f816b1a88bc78
Rashid <rashidgaitov@gmail.com>
2025-02-17 14:11:00 +0300

    Added Task Model

---
+++ ToDoList/Entities/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  ToDoList
+//
+//  Created by Rashid Gaitov on 17.02.2025.
+//
+
+import Foundation
+
+struct Task {
+    var title: String
+    var description: String
+    var creationDate: Date
+    var isCompleted: Bool
+}
