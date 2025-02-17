4c7a581393a17282c3a8445c3dfc211f2355590a
Rashid <rashidgaitov@gmail.com>
2025-02-17 14:21:32 +0300

    Added Router

---
+++ ToDoList/Routers/TaskListRouter.swift
@@ -0,0 +1,13 @@
+//
+//  TaskListRouter.swift
+//  ToDoList
+//
+//  Created by Rashid Gaitov on 17.02.2025.
+//
+
+import Foundation
+import UIKit
+
+final class TaskListRouterImplementation: TaskListRouter {
+    weak var viewController: UIViewController?
+}
