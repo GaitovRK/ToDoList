6b1f7fdaac3a8c68611bae97f9ac5821c8557195
Rashid <rashidgaitov@gmail.com>
2025-02-21 15:56:21 +0300

    Add CoreData: (save and fetch functions)

--- ToDoList/Data/TasksRepository.swift
+++ ToDoList/Data/TasksRepository.swift
@@ -20,9 +20,48 @@ protocol TasksRepositoryProtocol {
 
 final class TasksRepository: TasksRepositoryProtocol {
     var networkService = NetworkService()
+    var coreDataService = CoreDataService()
+    
+//    var tasks: [Task]
     
     func fetchTasks(completion: @escaping ([Task]) -> Void) {
-        networkService.get(path: nil, completion: completion)
+        //1. Check if it is the first launch
+        let didLaunchBefore = UserDefaults.standard.bool(forKey: "isFirstLaunch")
+        
+        if (!didLaunchBefore) {
+            print("1 launch")
+            networkService.get(path: nil) { [weak self] tasks in
+                guard let self = self else { return }
+                
+                //2. Save to Core Data
+                for task in tasks {
+                    let taskData: [String: Any] = [
+                        "id": task.id,
+                        "title": task.title,
+                        "isCompleted": task.isCompleted,
+                        "creationDate": task.creationDate,
+                        "taskDescription": task.description,
+                    ]
+                    self.coreDataService.addEntity(ofType: TaskCoreData.self, withData: taskData)
+                }
+                
+                //3. Return tasks
+                completion(tasks)
+            }
+        } else {
+            print("2+ launch")
+            let tasksData = self.coreDataService.fetchEntities(ofType: TaskCoreData.self)
+            var tasks: [Task]
+            
+            tasks = tasksData.map({ task in
+                Task(coreDataTask: task)
+            })
+            
+            completion(tasks)
+            UserDefaults.standard.set(false, forKey: "didLaunchBefore")
+        }
+        
+        
     }
     
 //    func searchTasks(query: String, completion: @escaping ([Task]) -> Void) {
