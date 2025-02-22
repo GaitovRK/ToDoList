a4587263a80ae45f2bca5cc0eec848b97fcd2fac
Rashid <rashidgaitov@gmail.com>
2025-02-22 12:33:32 +0300

    Added bottom Bar and New Task Button

--- ToDoList/UI/TaskList/TaskListViewController.swift
+++ ToDoList/UI/TaskList/TaskListViewController.swift
@@ -13,13 +13,11 @@ protocol TaskListView: AnyObject {
 }
 
 final class TaskListViewController: UIViewController, TaskListView, UISearchResultsUpdating {
-    func show(tasks: [Task]) {
-        self.tasks = tasks
-        self.tableView.reloadData()
-    }
     
     var presenter: TaskListPresenter
     private let tableView = UITableView()
+    private let bottomBar = UIView()
+    private let numberOfTasksLabel = UILabel()
     private var tasks: [Task] = []
     private var filteredTasks: [Task] = []
     private let searchController = UISearchController(searchResultsController: nil)
@@ -29,6 +27,7 @@ final class TaskListViewController: UIViewController, TaskListView, UISearchResu
         super.init(nibName: nil, bundle: nil)
         setupTableView()
         setupSearchController()
+        setupBottomBar()
     }
     
     required init?(coder: NSCoder) {
@@ -41,6 +40,12 @@ final class TaskListViewController: UIViewController, TaskListView, UISearchResu
  
         title = "Задачи"
         tableView.reloadData()
+    }
+    
+    func show(tasks: [Task]) {
+        self.tasks = tasks
+        self.tableView.reloadData()
+        numberOfTasksLabel.text = "\(getNumberOfTasks()) Задач"
     }
 
     private func setupSearchController() {
@@ -69,32 +74,44 @@ final class TaskListViewController: UIViewController, TaskListView, UISearchResu
         }
         tableView.reloadData()
     }
+    
+    func setupBottomBar() {
+        bottomBar.backgroundColor = .secondarySystemBackground // Set a background color for visibility
+        bottomBar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bottomBar)
 
-    // MARK: - UITableViewDataSource
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchController.isActive ? filteredTasks.count : tasks.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath) as? TaskTableViewCell else {
-            return UITableViewCell()
-        }
-        let task = searchController.isActive ? filteredTasks[indexPath.row] : tasks[indexPath.row]
-        cell.configure(with: task)
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        tableView.deselectRow(at: indexPath, animated: true)
-//        let taskIndex = searchController.isActive ? tasks.firstIndex(where: { $0.id == filteredTasks[indexPath.row].id }) : indexPath.row
-//        if let taskIndex = taskIndex {
-//            tasks[taskIndex].isCompleted.toggle()
-//            tableView.reloadRows(at: [indexPath], with: .automatic)
-//        }
+        numberOfTasksLabel.font = .systemFont(ofSize: 11)
+        numberOfTasksLabel.translatesAutoresizingMaskIntoConstraints = false
+        bottomBar.addSubview(numberOfTasksLabel)
         
-        let task = searchController.isActive ? filteredTasks[indexPath.row] : tasks[indexPath.row]
+        let addTaskButton = UIButton(type: .system)
+        addTaskButton.setImage(UIImage(systemName: "square.and.pencil"), for: .normal)
+        addTaskButton.translatesAutoresizingMaskIntoConstraints = false
+        addTaskButton.addTarget(self, action: #selector(addTaskButtonTapped), for: .touchUpInside)
+        bottomBar.addSubview(addTaskButton)
+        
+        NSLayoutConstraint.activate([
+            bottomBar.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            bottomBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bottomBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bottomBar.heightAnchor.constraint(equalToConstant: 50),
+            
+            numberOfTasksLabel.centerXAnchor.constraint(equalTo: bottomBar.centerXAnchor),
+            numberOfTasksLabel.centerYAnchor.constraint(equalTo: bottomBar.centerYAnchor),
+            
+            addTaskButton.centerXAnchor.constraint(equalTo: bottomBar.trailingAnchor, constant: -34),
+            addTaskButton.centerYAnchor.constraint(equalTo: bottomBar.centerYAnchor),
+            addTaskButton.heightAnchor.constraint(equalToConstant: 28),
+            addTaskButton.widthAnchor.constraint(equalToConstant: 28)
+        ])
+    }
+    
+    @objc func addTaskButtonTapped() {
+        let uniqueID = (tasks.last?.id ?? 0) + 1
+        let task = Task(id: uniqueID, title: "Новая Задача", description: "", creationDate: Date(), isCompleted: false)
         presenter.showTaskDetailView(navigationController: navigationController!, task: task)
     }
+
 }
 
 extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
@@ -110,6 +127,35 @@ extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(TaskTableViewCell.self, forCellReuseIdentifier: "TaskCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return getNumberOfTasks()
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath) as? TaskTableViewCell else {
+            return UITableViewCell()
+        }
+        let task = searchController.isActive ? filteredTasks[indexPath.row] : tasks[indexPath.row]
+        cell.configure(with: task)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+//        let taskIndex = searchController.isActive ? tasks.firstIndex(where: { $0.id == filteredTasks[indexPath.row].id }) : indexPath.row
+//        if let taskIndex = taskIndex {
+//            tasks[taskIndex].isCompleted.toggle()
+//            tableView.reloadRows(at: [indexPath], with: .automatic)
+//        }
+        
+        let task = searchController.isActive ? filteredTasks[indexPath.row] : tasks[indexPath.row]
+        presenter.showTaskDetailView(navigationController: navigationController!, task: task)
+    }
+    
+    func getNumberOfTasks() -> Int{
+        return searchController.isActive ? filteredTasks.count : tasks.count
     }
 }
 
--- ToDoList/UI/TaskList/View/TaskTableViewCell.swift
+++ ToDoList/UI/TaskList/View/TaskTableViewCell.swift
@@ -103,4 +103,8 @@ class TaskTableViewCell: UITableViewCell {
             checkboxButton.setImage(UIImage(systemName: "circle"), for: .normal)
             dateLabel.text = nil
         }
+    
+    func checkmarkPressed() {
+        
+    }
 }
